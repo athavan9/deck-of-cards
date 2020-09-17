396f5044d8ecdf28694c91afccd42d462ff2fd4a
Athavan Srividiyakaran <athavan@athavans-mbp.mynet>
2020-09-17 10:24:52 +0100

    Added drawCard test

--- deck-of-cards.test.js
+++ deck-of-cards.test.js
@@ -1,4 +1,4 @@
-const { shuffleDeck } = require('./deck-of-cards-api.js');
+const { shuffleDeck, drawCard } = require('./deck-of-cards-api.js');
 
 const axios = require('axios')
 jest.mock('axios');
@@ -45,4 +45,26 @@ test('shuffleDeck call api and get a deck id' , async () => {
   expect(axios.get).toBeCalledTimes(1)
   expect(axios.get).toBeCalledWith('blah/deck/new/shuffle/?deck_count=1')
 
+});
+
+test('drawCard calls api with deckID and gets a card' , async () => {
+    // Arrange
+    const expectedOutput = 'HA';
+    const deckID = '123'
+
+    axios.get.mockResolvedValue({
+      data: {
+        cards: [
+          expectedOutput,
+        ],
+      }
+    });
+
+    // Act
+    const actualOutput = await drawCard('https://deckofcardsapi.com/api/', deckID)
+    // Assert
+    expect(actualOutput).toBe(expectedOutput)
+    expect(axios.get).toBeCalledTimes(1)
+    expect(axios.get).toBeCalledWith(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`);
+  
 });

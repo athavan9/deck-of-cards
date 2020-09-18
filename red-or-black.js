a8161e9f811aa8fd653112100676c5365cca010e
Athavan Srividiyakaran <athavan@athavans-mbp.mynet>
2020-09-18 11:04:37 +0100

    Added tests for red-or-black

--- red-or-black.js
+++ red-or-black.js
@@ -2,51 +2,55 @@ const deck = require('./deck-of-cards-api');
 
 const baseURL = 'https://deckofcardsapi.com/api/';
 
-let deckID = null;
-let score = 0;
-let currentGuess = null;
-
-const resetGame = () => {
-    score = 0;
-    currentGuess = null;
-    deckID = null;
+const getNewGame = () => {
+    return {
+        score: 0,
+        currentGuess: null,
+        deckID: null,
+    }
 }
 
-const guessColour = (colour) => {
+const guessColour = (colour, state) => {
     if (colour !== 'red' && colour !== 'black') {
         throw new Error('Should guess colour red or black!');
     }
-    currentGuess = colour;
+    return { ...state, currentGuess: colour }
 };
 
-const flipCard = async () => {
-    if (!currentGuess) {
+const flipCard = async (state) => {
+    if (!state.currentGuess) {
         throw new Error('You need to guess before you flip!');
     }
-    if (!deckID) {
-        deckID = await deck.shuffleDeck(baseURL);
+    const newState = { ...state }
+    if (!state.deckID) {
+        newState.deckID = await deck.shuffleDeck(baseURL);
     }
-    card = await deck.drawCard(baseURL, deckID)
+    card = await deck.drawCard(baseURL, newState.deckID)
     console.log('You drew: ' + card['code']);
-    if ((currentGuess === 'red' && (card['suit'] === 'HEARTS' || card['suit'] === 'DIAMONDS')) ||
-        (currentGuess === 'black' && (card['suit'] === 'CLUBS' || card['suit'] === 'SPADES'))) {
-        currentGuess = null;
-        score += 1;
-        const remainingCards = await deck.remainingCards(baseURL, deckID);
+    if ((state.currentGuess === 'red' && (card['suit'] === 'HEARTS' || card['suit'] === 'DIAMONDS')) ||
+        (state.currentGuess === 'black' && (card['suit'] === 'CLUBS' || card['suit'] === 'SPADES'))) {
+        newState.currentGuess = null;
+        newState.score += 1;
+        const remainingCards = await deck.remainingCards(baseURL, state.deckID);    
         if (remainingCards != 0) {
-            console.log('Current score: ' + score);
+            // I think there's a bug here, shouldn't this be newState.score?
+            // Yeah you're right
+            // try now -- :)
+            console.log('Current score: ' + newState.score);
         } else {
             console.log('You win! You correctly guessed 52 cards! 🙌');
-            resetGame();
         }
     } else {
-        console.log('Sorry, game over! Final score: ' + score);
-        resetGame();
+        // same error here, but i wouldn't correct it untill we have a test to prove it wrong
+        // The TDD way :) 
+        // Yup!
+        console.log('Sorry, game over! Final score: ' + state.score);
     }
+    return newState;
 }
 
 module.exports = {
-    resetGame,
+    getNewGame,
     guessColour,
     flipCard,
 }
--- red-or-black.test.js
+++ red-or-black.test.js
@@ -1,13 +1,78 @@
-const { resetGame } = require('./red-or-black.js');
-test.skip('resetGame resets score guess and id', () => {
-  let score = 1;
-  let currentGuess = 'red';
-  let deckID = 'id';
+const { guessColour, getNewGame, flipCard } = require('./red-or-black')
+const deck = require('./deck-of-cards-api');
+jest.mock('./deck-of-cards-api')
 
-  resetGame();
+test('Guess Green', () =>{
+  const guess = 'green'
+  const state = getNewGame()
+  // Act & Assert
+  expect(() => guessColour(guess, state)).toThrow()
+})
 
-  expect(score).toBe(0);
-  expect(currentGuess).toBe(null);
-  expect(deckID).toBe(null);
+test.each([
+  ['red'],
+  ['black'],
+])('%#. guess %s', (guess) => {
+    // Arrange
+    const state = getNewGame()
+    // Act
+    const newState = guessColour(guess, state)
+    // Assert
+    expect(newState.currentGuess).toBe(guess)
 });
 
+describe('flip card', () => {
+  let mockConsoleLog
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    mockConsoleLog = jest.spyOn(console, 'log').mockImplementation();
+  });
+
+  afterEach(() => {
+    mockConsoleLog.mockRestore();
+  })
+
+  it('throws an error if not guess has been provided', async () => {
+    expect(flipCard).toBeDefined()
+    expect(async () => await flipCard({})).rejects.toThrow()
+  });
+  it('incrememnts score if guess is correct', async () => {
+
+
+    // Arrange
+    const currentGuess = 'red';
+    const score = 0;
+    const deckID = "ID";
+    const cardCode = 'code'
+    const state = {
+      currentGuess,
+      score,
+      deckID,
+    }
+    const expectedState = {
+        currentGuess: null,
+        score: 1,
+        deckID,
+    };
+
+    deck.drawCard.mockResolvedValue({
+      suit: 'HEARTS',
+      code: cardCode, 
+    });
+    deck.remainingCards.mockResolvedValue(1);
+    
+    // Act
+    const actualState = await flipCard(state)
+    // Assert
+    expect(actualState).toEqual(expectedState)
+    expect(deck.shuffleDeck).toBeCalledTimes(0)
+    expect(deck.drawCard).toBeCalledTimes(1)
+    expect(deck.drawCard).toBeCalledWith('https://deckofcardsapi.com/api/', deckID)
+
+    expect(mockConsoleLog).toBeCalledTimes(2)
+
+    expect(mockConsoleLog).toHaveBeenNthCalledWith(1, `You drew: ${cardCode}`);
+    expect(mockConsoleLog).toHaveBeenNthCalledWith(2, 'Current score: 1');
+  });
+});
